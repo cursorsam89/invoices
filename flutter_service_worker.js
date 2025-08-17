91f021f5ef257259843a5fbf7928ff4f72257cd9
shreyai347 <shreyai347@users.noreply.github.com>
2025-08-17 18:03:32 +0000

    deploy: aa246f6bda0652f746e3b34490c72639963355cb

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bb3cb711543d48a67dedebc285b0ae13",
+const RESOURCES = {"flutter_bootstrap.js": "2d2fb6af4defb240cf0c450d7872f26e",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
