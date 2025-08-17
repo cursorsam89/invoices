1d277a845fea8d3e8aa7ecdb7c61064ce41d65ac
shreyai347 <shreyashreya2322@gmail.com>
2025-08-17 14:41:29 +0530

    updated files

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,7 +38,5 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "1774031606"
-  }
+  serviceWorkerSettings: null
 });
